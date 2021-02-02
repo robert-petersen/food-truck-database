19f75dc482aeb93ba362f381eaeba397f2024271
Robert Petersen <robert.petersen808@gmail.com>
2021-02-02 16:38:52 -0600

    knex updated

--- knexfile.js
+++ knexfile.js
@@ -4,31 +4,31 @@ const DB = process.env.DB;
 
 module.exports = {
 
-  development: {
-    client: 'sqlite3',
-    useNullAsDefault: true,
-    connection: {
-      filename: './database/foodtruck.db3'
-    },
-    pool: {
-      afterCreate: (conn, done) => {
-        conn.run("PRAGMA foreign_keys = ON", done);
-      },
-    },
-    migrations: {
-      directory: "./database/migrations",
-    },
-    seeds: {
-      directory: "./database/seeds",
-    },
-  },
+  // development: {
+  //   client: 'sqlite3',
+  //   useNullAsDefault: true,
+  //   connection: {
+  //     filename: './database/foodtruck.db3'
+  //   },
+  //   pool: {
+  //     afterCreate: (conn, done) => {
+  //       conn.run("PRAGMA foreign_keys = ON", done);
+  //     },
+  //   },
+  //   migrations: {
+  //     directory: "./database/migrations",
+  //   },
+  //   seeds: {
+  //     directory: "./database/seeds",
+  //   },
+  // },
 
   production: {
     client: "pg",
     connection: DB,
     migrations: {
-      directory: "./data/migrations",
+      directory: "./database/migrations",
     },
-    seeds: { directory: "./data/seeds" },
+    seeds: { directory: "./database/seeds" },
   }
 };
